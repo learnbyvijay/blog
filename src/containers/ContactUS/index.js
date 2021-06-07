b4aa8062eaab02e9b0bc289071fc903207ce5314
unknown <vijaykasundra99@gmail.com>
2021-06-06 21:47:55 -0700

    hii

---
+++ src/containers/ContactUS/index.js
@@ -0,0 +1,25 @@
+import React from 'react'
+
+/**
+* @author
+* @function ContactUS
+**/
+
+const ContactUS = (props) => {
+  return(
+    <div className="form" >
+    	<form>
+    	<label for="EMAIL ID">Name</label><br/>
+        <input type="text" name="Email id" id="Email id" /><br/>
+        <label for="EMAIL ID">Email Id:</label><br/>
+        <input type="text" name="Email id" id="Email id" /><br/>
+        <input type="submit" name="Email id" id="Email id" />
+        </form>
+
+
+    </div>
+   )
+
+ }
+
+export default ContactUS
